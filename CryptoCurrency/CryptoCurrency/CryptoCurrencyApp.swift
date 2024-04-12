cb6db6bf6cb0c6e7be40e16dbc82a0235ed5a3c5
Oleksandr Isaiev <oleksandr.isaiev@thetrainline.com>
2024-04-12 12:44:13 +0300

    init project

---
+++ CryptoCurrency/CryptoCurrency/CryptoCurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoCurrencyApp.swift
+//  CryptoCurrency
+//
+//  Created by Oleksandr Isaiev on 12.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct CryptoCurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
