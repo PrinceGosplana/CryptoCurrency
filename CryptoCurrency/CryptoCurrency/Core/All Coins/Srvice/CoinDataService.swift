512e255a34c89b2260eae4b1db062b84cc6bdb9d
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-04-13 18:41:47 +0300

    add error handling into request

--- CryptoCurrency/CryptoCurrency/Core/All Coins/Srvice/CoinDataService.swift
+++ CryptoCurrency/CryptoCurrency/Core/All Coins/Srvice/CoinDataService.swift
@@ -11,19 +11,37 @@ final class CoinDataService {
 
     private let urlString = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=30&page=1&sparkline=false&price_change_percentage=24&locale=en"
 
-    func fetchCoins(completion: @escaping (Result<[Coin], Error>) -> Void) {
+    func fetchCoins(completion: @escaping (Result<[Coin], CoinAPIError>) -> Void) {
         guard let url = URL(string: urlString) else { return }
 
         URLSession.shared.dataTask(with: url) { data, response, error in
 
             if let error {
-                completion(.failure(error))
+                completion(.failure(.unknownError(error: error)))
                 return
             }
-            guard let data else { return }
-            
-            guard let coins = try? JSONDecoder().decode([Coin].self, from: data) else { return }
-            completion(.success(coins))
+
+            guard let httpResponse = response as? HTTPURLResponse else {
+                completion(.failure(.requestFailed(description: "Request failed")))
+                return
+            }
+
+            guard httpResponse.statusCode == 200 else {
+                completion(.failure(.invalidStatusCode(statusCode: httpResponse.statusCode)))
+                return
+            }
+
+            guard let data else {
+                completion(.failure(.invalidData))
+                return
+            }
+
+            do {
+                let coins = try JSONDecoder().decode([Coin].self, from: data)
+                completion(.success(coins))
+            } catch {
+                completion(.failure(.jsonParsingFailure))
+            }
         }.resume()
     }
 }
