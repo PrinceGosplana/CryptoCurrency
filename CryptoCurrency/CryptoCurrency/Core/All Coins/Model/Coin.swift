b3615098896c998fafad0bda6d1124c47ed4ce88
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-04-13 15:05:14 +0300

    extend model and show inside list

--- CryptoCurrency/CryptoCurrency/Core/All Coins/Model/Coin.swift
+++ CryptoCurrency/CryptoCurrency/Core/All Coins/Model/Coin.swift
@@ -11,4 +11,15 @@ struct Coin: Codable, Identifiable {
     let id: String
     let symbol: String
     let name: String
+    let currentPrice: Double
+    let marketCapRank: Int
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case symbol
+        case name
+        case currentPrice = "current_price"
+        case marketCapRank = "market_cap_rank"
+    }
+
 }
